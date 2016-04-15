3ea8faf504d57a85e7cb0d722bc73bea144d9f1d
thurler <gt@tgr.net.br>
2016-04-15 17:55:38 -0300

    Finalized basic modules, tested all current functions

--- pyimage.py
+++ pyimage.py
@@ -13,15 +13,23 @@ class PyImage(object):
     # Input and Output functions
     # ------------------------------------------------------------------------
 
+    def init(self):
+
+        '''This function should...'''
+
+        self.pixels = np.array(self.img)
+
+        self.width = self.img.size[0]
+        self.height = self.img.size[1]
+
     def loadImage(self, image):
 
         '''This function should...'''
 
         if isinstance(image, Image.Image):
             self.img = image
-            self.pixels = np.array(image.getdata())
-            self.width = self.img.size[0]
-            self.height = self.img.size[1]
+            self.init()
+
         else:
             print "\nERROR: Argument is not an instance of Image class\n"
 
@@ -31,9 +39,8 @@ class PyImage(object):
 
         try:
             self.img = Image.open(filepath)
-            self.pixels = np.array(self.img.getdata())
-            self.width = self.img.size[0]
-            self.height = self.img.size[1]
+            self.init()
+
         except IOError:
             print "\nERROR: File not found.\n"
 
@@ -42,50 +49,10 @@ class PyImage(object):
         '''This function should...'''
 
         try:
-            self.img.putdata(self.pixels)
-            self.img.save(filepath)
+            Image.fromarray(self.pixels, self.img.mode).save(filepath)
+
         except IOError:
             print "\nERROR: File could not be saved.\n"
-
-    # ------------------------------------------------------------------------
-    # Channel splitting functions
-    # ------------------------------------------------------------------------
-
-    def redChannel(self):
-
-        '''This function should...'''
-
-        if self.img.mode not in ['RGB', 'RGBA']:
-            print "\nERROR: Image does not have a red channel\n"
-            return
-
-        channel = PyImage()
-        channel.loadImage(self.img.split()[0])
-        return channel
-
-    def greenChannel(self):
-
-        '''This function should...'''
-
-        if self.img.mode not in ['RGB', 'RGBA']:
-            print "\nERROR: Image does not have a green channel\n"
-            return
-
-        channel = PyImage()
-        channel.loadImage(self.img.split()[1])
-        return channel
-
-    def blueChannel(self):
-
-        '''This function should...'''
-
-        if self.img.mode not in ['RGB', 'RGBA']:
-            print "\nERROR: Image does not have a blue channel\n"
-            return
-
-        channel = PyImage()
-        channel.loadImage(self.img.split()[2])
-        return channel
 
     # ------------------------------------------------------------------------
     # Pixel reading and writing
@@ -95,45 +62,33 @@ class PyImage(object):
 
         '''This function should...'''
 
-        if (x < 0 or x >= self.width - 1):
+        if (x < 0 or x > self.width - 1):
             print "\nERROR: Pixel X coordinate out of range\n"
             return
 
-        if (y < 0 or y >= self.height - 1):
+        if (y < 0 or y > self.height - 1):
             print "\nERROR: Pixel Y coordinate out of range\n"
             return
 
-        index = x * self.width + y
-
-        return self.pixels[index]
+        return self.pixels[y][x]
 
     def setPixel(self, x, y, value):
 
-        '''This functions should...'''
+        '''This function should...'''
 
-        if (x < 0 or x >= self.width - 1):
+        if (x < 0 or x > self.width - 1):
             print "\nERROR: Pixel X coordinate out of range\n"
             return
 
-        if (y < 0 or y >= self.height - 1):
+        if (y < 0 or y > self.height - 1):
             print "\nERROR: Pixel Y coordinate out of range\n"
             return
 
-        index = x * self.width + y
-
-        pixels[index] = value
+        self.pixels[y][x] = value
 
     # ------------------------------------------------------------------------
     # Filters
     # ------------------------------------------------------------------------
-
-    def convertBinary(self, threshold):
-
-        '''This function should...'''
-
-        self.pixels /= threshold
-        self.pixels /= self.pixels
-        self.pixels *= 255
 
     def negate(self):
 
@@ -151,4 +106,4 @@ class PyImage(object):
 
         power = 1.0 / gamma
 
-        self.pixels = (255 * ((self.pixels / 255) ** power)) // 1
+        self.pixels = (((self.pixels / 255.0) ** power) * 255).astype("uint8")
