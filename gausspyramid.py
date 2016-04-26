dcd1b0f12ec208af60f29777bd4be9ca59589f1e
thurler <gt@tgr.net.br>
2016-04-26 10:02:52 -0300

    Added documentation for gauss pyramid

--- gausspyramid.py
+++ gausspyramid.py
@@ -6,7 +6,10 @@ import numpy as np
 
 class GaussPyramid(object):
 
-    '''This class...'''
+    '''This class represent a gaussian pyramid of a given image. It contains
+    an array of PyImage instances, where the higher the index, the deeper you
+    are in the pyramid; and also an array of loss information, so that we can
+    prevent pixel lines and columns from being lost when reducing an image.'''
 
     def __init__(self):
 
@@ -19,37 +22,55 @@ class GaussPyramid(object):
 
     def loadFile(self, filepath):
 
-        '''This function should...'''
+        '''This function should initialize the class by loading an image from
+        the path specified. It then places that image at the pyramid's lowest
+        level, and specifies no pixels were lost in this process.'''
 
-        if not self.pyramid:
+        # Reset pyramid and loss info
+        if self.pyramid:
             self.pyramid = []
+            self.info_loss = []
 
-        self.img = PyImage()
-        self.img.loadFile(filepath)
-        self.pyramid.append(self.img)
+        # Create image, append things
+        img = PyImage()
+        img.loadFile(filepath)
+        self.pyramid.append(img)
         self.info_loss.append((False, False))
 
     def loadImage(self, image):
 
-        '''This function should...'''
+        '''This function should initialize the class by loading an image from
+        the function call. It then places that image at the pyramid's lowest
+        level, and specifies no pixels were lost in this process.'''
 
-        if not self.pyramid:
+        # Reset pyramid and loss info
+        if self.pyramid:
             self.pyramid = []
+            self.info_loss = []
 
-        self.img = PyImage()
-        self.img.loadImage(image)
-        self.pyramid.append(self.img)
+        # Create image, append things
+        img = PyImage()
+        img.loadImage(image)
+        self.pyramid.append(img)
         self.info_loss.append((False, False))
 
     def savePyramid(self, filepath):
 
-        '''This function should...'''
+        '''This function should save the images in the pyramid into different
+        files, following the filename given in the function call. Images are
+        saved in format "<name>-<level>.<extension>", where <name> and
+        <extension> are given with filepath, and <level> specifies which level
+        is being saved.'''
 
+        # Level counter, starts at lowest level [0]
         count = 0
+
+        # Separate name from extension
         path = filepath.split('.')
         extension = '.' + path[-1]
         path = "".join(path[:-1]) + '-'
 
+        # Save each level separately
         for image in self.pyramid:
             image.saveFile(path + str(count) + extension)
             count += 1
@@ -60,31 +81,39 @@ class GaussPyramid(object):
 
     def reduceMax(self):
 
-        '''This function should...'''
-
-        size = self.pyramid[-1].img.size
+        '''This function should reduce the image a fixed number of times. This
+        adds a specific amount of levels to the gaussian pyramid.'''
 
         for i in range(4):
             self.reduce()
-            size = self.pyramid[-1].img.size
 
     def reduce(self):
 
-        '''This function should...'''
+        '''This function should reduce the image by a single level. This adds a
+        new level to the pyramid, so the reducing is based on the current
+        highest level. Before halving the image's dimensions, we blur it to
+        prevent sharpening pixel intensity differences.'''
 
+        # Check if pyramid has been generated
         if not self.pyramid:
             print "\nERROR: Please load an image first\n"
             return
 
+        # Copy highest level's pixel matrix
         img = self.pyramid[-1].copy()
 
+        # Blur filter we should apply to the imag
         arr = np.array([1, 4, 6, 4, 1]) / 16.0
 
+        # To create a proper 5x5 filter, we create the weights matrix, taking
+        # into account the number of channels in the image
         if img.pixels.ndim < 3:
             weights = np.empty((5, 5))
         else:
             weights = np.empty((5, 5, len(img.pixels[0][0])))
 
+        # Then, we convolve the 1D filter with itself, creating a proper 5x5
+        # filter we can use with our generic filter function
         for i in range(5):
             for j in range(5):
                 if img.pixels.ndim < 3:
@@ -94,6 +123,8 @@ class GaussPyramid(object):
 
         img.filter(2, weights, np.sum)
 
+        # We now check if any pixels will be lost when halving the image - this
+        # happens when we have an odd dimension
         loss = []
         if len(img.pixels) % 2:
             loss.append(True)
@@ -105,24 +136,32 @@ class GaussPyramid(object):
         else:
             loss.append(False)
 
+        # Half the image, taking lines and columns alternatedly
         img.pixels = img.pixels[:-1:2, :-1:2]
         img.updateImage()
 
+        # Append new stuff to new level
         self.pyramid.append(img)
         self.info_loss.append(loss)
 
     def expand(self, level):
 
-        '''This function should...'''
+        '''This function should expand the image stored at the specified level.
+        To do so, we simply call the expand function for that image, specifying
+        whether there was a loss of pixels or not in its reduction to that
+        level.'''
 
+        # Check if pyramid exists
         if not self.pyramid:
             print "\nERROR: Please load an image first\n"
             return
 
+        # Check if index is not negative
         if level < 0:
             print "\nERROR: Please use non-negative index values\n"
             return
 
+        # Check if index is valid
         try:
             img = self.pyramid[level].copy()
             loss = self.info_loss[level]
@@ -131,5 +170,6 @@ class GaussPyramid(object):
             print "\nERROR: Please specify a valid index\n"
             return
 
+        # Expand image
         img.expand(loss)
         return img
