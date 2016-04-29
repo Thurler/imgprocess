84638d333fe9ad2dfc780c422f9d2f874878d06d
thurler <gt@tgr.net.br>
2016-04-29 18:09:20 -0300

    Added docs for laplace pyramid and blender

--- laplaceblender.py
+++ laplaceblender.py
@@ -8,7 +8,11 @@ import numpy as np
 
 class LaplaceBlender(object):
 
-    '''This class...'''
+    '''This class represents the Laplacian Pyramid Blending process in a data
+    structure containing two laplacian pyramids, for two given images, each of
+    those with a gaussian pyramid as well, a third gaussian pyramid for a given
+    mask that will be used to blend, and a third laplacian pyramid for the
+    blending of the other two laplacian pyramids, given the mask.'''
 
     def __init__(self, reduce_default=4):
 
@@ -24,14 +28,22 @@ class LaplaceBlender(object):
 
     def defaultMask(self, size, mode):
 
-        '''This function should...'''
+        '''This function should generate a default mask image and return it.
+        The image generated has size and mode specified as arguments. The
+        default mask is a binary mask that is white in the left half and black
+        in the right half.'''
 
+        # Invert x,y for numpy
         size = (size[1], size[0])
+        # Set matrix of zeros
         mask = np.zeros(size)
+        # Fill rightmost half with ones
         mask[:, :size[1]/2] = np.ones((size[0],
                                        size[1]/2))
+        # Multiply everything by 255
         mask *= 255
 
+        # Convert image to grayscale mode, then the desired mode, and return
         return Image.fromarray(mask.astype("uint8"), 'L').convert(mode)
 
     # ------------------------------------------------------------------------
@@ -40,18 +52,23 @@ class LaplaceBlender(object):
 
     def loadFiles(self, filepath_a, filepath_b, filepath_mask=None):
 
-        '''This function should...'''
+        '''This function should load at least two files that contain images to
+        be used in the blending. A third file can be passed in to be used as
+        mask. This function should initiallize laplacian pyramids for both
+        mandatory images, and a gaussian pyramid for the mask. If a mask is not
+        given, a default one is generated.'''
 
+        # Load files into laplacian pyramids
         self.l_pyramid_a.loadFile(filepath_a)
         self.l_pyramid_b.loadFile(filepath_b)
-        self.l_pyramid_a.buildPyramid()
-        self.l_pyramid_b.buildPyramid()
 
+        # Get size and mode for both of them
         size_a = self.l_pyramid_a.gauss_pyramid.pyramid[0].img.size
         size_b = self.l_pyramid_b.gauss_pyramid.pyramid[0].img.size
         mode_a = self.l_pyramid_a.gauss_pyramid.pyramid[0].img.mode
         mode_b = self.l_pyramid_b.gauss_pyramid.pyramid[0].img.mode
 
+        # Assert images have same size and pixel mode
         if size_a != size_b:
             print "\nERROR: The two images do not have the same size\n"
             return
@@ -60,12 +77,18 @@ class LaplaceBlender(object):
             print "\nERROR: The images do not have same number of channels\n"
             return
 
+        # Build the laplacian pyramid for both images
+        self.l_pyramid_a.buildPyramid()
+        self.l_pyramid_b.buildPyramid()
+
         if filepath_mask is None:
+            # Generate default mask and generate gaussian pyramid for it
             img = self.defaultMask(size_a, mode_a)
             self.g_pyramid_mask.loadImage(img)
             self.g_pyramid_mask.reduceMax()
 
         else:
+            # Read mask from disk and generate gaussian pyramid for it
             self.g_pyramid_mask.loadFile(filepath_mask)
             self.g_pyramid_mask.pyramid[-1].img = \
                 self.g_pyramid_mask.pyramid[-1].img.convert(mode_a)
@@ -74,18 +97,23 @@ class LaplaceBlender(object):
 
     def loadImages(self, image_a, image_b, image_mask=None):
 
-        '''This function should...'''
+        '''This function should load at least two images that contain images to
+        be used in the blending. A third image can be passed in to be used as
+        mask. This function should initiallize laplacian pyramids for both
+        mandatory images, and a gaussian pyramid for the mask. If a mask is not
+        given, a default one is generated.'''
 
+        # Load files into laplacian pyramids
         self.l_pyramid_a.loadImage(image_a)
         self.l_pyramid_b.loadImage(image_b)
-        self.l_pyramid_a.buildPyramid()
-        self.l_pyramid_b.buildPyramid()
 
+        # Get size and mode for both of them
         size_a = self.l_pyramid_a.gauss_pyramid.pyramid[0].img.size
         size_b = self.l_pyramid_b.gauss_pyramid.pyramid[0].img.size
         mode_a = self.l_pyramid_a.gauss_pyramid.pyramid[0].img.mode
         mode_b = self.l_pyramid_b.gauss_pyramid.pyramid[0].img.mode
 
+        # Assert images have same size and pixel mode
         if size_a != size_b:
             print "\nERROR: The two images do not have the same size\n"
             return
@@ -94,24 +122,32 @@ class LaplaceBlender(object):
             print "\nERROR: The images do not have same number of channels\n"
             return
 
+        # Build the laplacian pyramid for both images
+        self.l_pyramid_a.buildPyramid()
+        self.l_pyramid_b.buildPyramid()
+
         if filepath_mask is None:
+            # Generate default mask and generate gaussian pyramid for it
             img = self.defaultMask(size_a, mode_a)
             self.g_pyramid_mask.loadImage(img)
             self.g_pyramid_mask.reduceMax()
 
         else:
+            # Read mask and generate gaussian pyramid for it
             image_mask.convert(mode_a)
             self.g_pyramid_mask.loadImage(image_mask)
             self.g_pyramid_mask.reduceMax()
 
     def saveFile(self, filepath):
 
-        '''This function should...'''
+        '''This function should save every pyramid it has into files.'''
 
+        # Separate filename from extension
         path = filepath.split('.')
         extension = '.' + path[-1]
         path = "".join(path[:-1]) + '-'
 
+        # Save all pyramids
         self.l_pyramid_a.savePyramid(path+'lpyramid-A'+extension)
         self.l_pyramid_b.savePyramid(path+'lpyramid-B'+extension)
         self.g_pyramid_mask.savePyramid(path+'gpyramid-Mask'+extension)
@@ -125,26 +161,32 @@ class LaplaceBlender(object):
 
     def blendPyramids(self):
 
-        '''This function should...'''
+        '''This function should blend pyramids A and B into pyramid BLEND.'''
 
+        # Assert pyramids have been loaded
         if not self.g_pyramid_mask.pyramid:
             print "\nERROR: Please load images onto the blender\n"
 
+        # Get image to start new pyramid's gaussian pyramid from pyramid A
         img = self.l_pyramid_a.gauss_pyramid.pyramid[-1]
 
+        # Blend it with the one from pyramid B using the corresponding mask
         img = img.blend(self.l_pyramid_b.gauss_pyramid.pyramid[-1],
                         self.g_pyramid_mask.pyramid[-1])
 
+        # Generate new laplace pyramid blending A and B
         self.l_pyramid_blend = self.l_pyramid_a.blend(self.l_pyramid_b,
                                                       self.g_pyramid_mask,
                                                       img)
 
     def collapse(self, filepath):
 
-        '''This function should...'''
+        '''This function should collapse the blended pyramid.'''
 
+        # Assert blended image is laoded
         if not self.l_pyramid_blend.pyramid:
             print "\nERROR: Please blend the image pyramids first\n"
 
+        # Collapse it
         self.l_pyramid_blend.collapsePyramid(filepath,
                                              self.g_pyramid_mask.info_loss)
--- laplacepyramid.py
+++ laplacepyramid.py
@@ -7,7 +7,10 @@ import numpy as np
 
 class LaplacePyramid(object):
 
-    '''This class...'''
+    '''This class represents a laplace pyramid of a given image. It contains
+    an array of PyImage instances, where the higher the index, the deeper you
+    are in the pyramid; it also contains a gaussian pyramid that, when paired
+    with the laplacian pyramid, allows collapsing onto the original image.'''
 
     def __init__(self, reduce_default=4):
 
@@ -21,37 +24,54 @@ class LaplacePyramid(object):
 
     def loadFile(self, filepath):
 
-        '''This function should...'''
+        '''This function should initialize the class by loading a guassian
+        pyramid from the image from the path specified. It then reduces that
+        pyramid as much as possible.'''
 
+        # Reset pyramid
         if not self.pyramid:
             self.pyramid = []
 
+        # Load gauss pyramid and reduce it
         self.gauss_pyramid.loadFile(filepath)
         self.gauss_pyramid.reduceMax()
 
     def loadImage(self, image):
 
-        '''This function should...'''
+        '''This function should initialize the class by loading a guassian
+        pyramid from the image specified. It then reduces that pyramid as much
+        as possible.'''
 
+        # Reset pyramid
         if not self.pyramid:
             self.pyramid = []
 
+        # Load gauss pyramid and reduce it
         self.gauss_pyramid.loadImage(image)
         self.gauss_pyramid.reduceMax()
 
     def savePyramid(self, filepath):
 
-        '''This function should...'''
+        '''This function should save the images in the pyramid into different
+        files, following the filename given in the function call. Images are
+        saved in format "<name>-<level>.<extension>", where <name> and
+        <extension> are given with filepath, and <level> specifies which level
+        is being saved.'''
 
+        # Level counter, starts at lowest level [0]
         count = 0
+
+        # Separate filename from extension
         path = filepath.split('.')
         extension = '.' + path[-1]
         path = "".join(path[:-1]) + '-'
 
+        # Save each level separately
         for image in self.pyramid:
             image.saveFile(path + str(count) + extension)
             count += 1
 
+        # Save the gaussian pyramid as well
         self.gauss_pyramid.savePyramid(path+'gpyramid'+extension)
 
     # ------------------------------------------------------------------------
@@ -60,50 +80,77 @@ class LaplacePyramid(object):
 
     def buildPyramid(self):
 
-        '''This function should...'''
+        '''This function should build the pyramid based on the gaussian pyramid
+        currently stored in the class. The procedure is, for every level except
+        the last one, we take level i and subtract it from the expanded level
+        i - 1.'''
 
+        # Reset pyramid
         self.pyramid = []
 
+        # For each level, subtract it from the next level's expansion
         for i in range(len(self.gauss_pyramid.pyramid) - 1):
             self.pyramid.append(self.gauss_pyramid.pyramid[i] -
                                 self.gauss_pyramid.expand(i+1))
 
     def collapsePyramid(self, filepath, loss=None):
 
-        '''This function should...'''
+        '''This function should collapse the pyramid, based on the image stored
+        at the highest level in the gaussian pyramid. We expand that and add
+        the result to the highest level in the laplace pyramid, and repeat the
+        process for every level below, until we reach the base. We save every
+        intermediate result.'''
 
+        # Initialize result as gaussian pyramid's highest level
         result = self.gauss_pyramid.pyramid[-1].copy()
 
+        # Count for saving images, we start at the highest level minus one
         count = len(self.pyramid) - 1
+
+        # Separate filename from extension
         path = filepath.split('.')
         extension = '.' + path[-1]
         path = "".join(path[:-1]) + '-'
 
         for level in self.pyramid[::-1]:
             if loss is not None:
+                # If loss array is specified, read loss from there
                 result.expand(loss[count+1])
             else:
+                # Else, read it from gaussian pyramid
                 result.expand(self.gauss_pyramid.info_loss[count+1])
+            # Add expansion to current laplacian pyramid level
             result = level + result
+            # Save file to disk
             result.saveFile(path + str(count) + extension)
             count -= 1
 
     def blend(self, other, mask, gauss):
 
-        '''This function should...'''
+        '''This function should blend two laplacian pyramids, self and other,
+        using a mask gaussian pyramid as reference. The function also takes in
+        an image to start the gauss pyramid of the new laplace pyramid, since
+        that cannot be empty. This function returns a new laplace pyramid,
+        which levels are the blending of each pyramid's levels.'''
 
+        # Check if all pyramids exist
         if not self.pyramid or not other.pyramid or not mask.pyramid:
             print "\nERROR: Please make sure all the pyramids are built\n"
             return
 
+        # Check if pyramids have same number of levels
         if (len(self.pyramid) != len(other.pyramid) or
                 len(self.pyramid) != len(mask.pyramid) - 1):
             print "\nERROR: Please make sure the pyramids have the same size\n"
             return
 
+        # Create new lapalce pyramid and store the given image at the new gauss
+        # pyramid's lowest level
         res = LaplacePyramid(self.reduce_default)
         res.gauss_pyramid.pyramid.append(gauss)
 
+        # For every level, the result pyramid is the blending of each pyramid's
+        # level
         for i in range(len(self.pyramid)):
             res.pyramid.append(self.pyramid[i].blend(other.pyramid[i],
                                                      mask.pyramid[i]))
